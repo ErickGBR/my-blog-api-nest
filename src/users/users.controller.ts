b46b1abcb5fb95e3cfae0b0721633b17175bbfa7
Erick Burgos <eburgosrivas1997@gmail.com>
2026-01-22 15:35:45 -0600

    feat: how add post and delete from nest.js

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,23 +1,54 @@
-import { Controller, Get } from '@nestjs/common';
+    import { Controller, Get, Body, Param, Post, Delete } from '@nestjs/common';
 
-interface User {
-    id: number;
-    name: string;
-    email: string;
-}
-
-@Controller('users')
-export class UsersController {
-    private users: User[] = [
-        {
-            id: 1,
-            name: 'Alice',
-            email: "alice@example.com"
-            }
-    ];
-
-    @Get()
-    getAllUsers(): User[] {
-        return this.users;
+    interface User {
+        id: number;
+        name: string;
+        email: string;
     }
-}
+
+    @Controller('users')
+    export class UsersController {
+        private users: User[] = [
+            {
+                id: 1,
+                name: 'Alice',
+                email: "alice@example.com"
+            },
+            {
+                id: 2,
+                name: 'Bob',
+                email: "bob@example.com"
+            }
+                
+        ];
+
+        @Get()
+        getAllUsers(): User[] {
+            return this.users;
+        }
+
+        @Get(":id")
+        findUser(@Param("id") id: string): User | { error: string } {
+            const user = this.users.find(user => user.id === parseInt(id));
+            if (!user) {
+                return {
+                    error: "User not found"
+                }
+            }
+            return user;
+        }
+
+        @Post()
+        createUser(@Body() body:User): User {
+            this.users.push(body);
+            return body;
+        }
+
+        @Delete(":id")
+        deleteUser(@Param("id") id: string): { message: string } {
+            this.users = this.users.findIndex(user => user.id === parseInt(id)) !== -1 ? this.users.filter(user => user.id !== parseInt(id)) : this.users;
+            return {
+                message: "User deleted successfully"
+            }
+        }
+    }
