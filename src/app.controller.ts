446fe694c1c778c482579833631416d6a28cd674
Erick Burgos <eburgosrivas1997@gmail.com>
2026-01-22 21:39:28 -0600

    feat: add module only user setting

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,4 @@
 import { Controller, Get } from "@nestjs/common";
-import { AppService } from "./app.service";
 import { UsersService } from "./users/users.service";
 import { ConfigService } from "@nestjs/config";
 import { Env } from "./env.model";
@@ -7,16 +6,14 @@ import { Env } from "./env.model";
 @Controller()
 export class AppController {
   constructor(
-    private readonly appService: AppService,
     private readonly usersService: UsersService,
     private readonly configService: ConfigService<Env>,
   ) {}
 
   @Get()
-  getHello(): string {
+  getHello(): void {
     const myVar = this.configService.get<string>("MY_VAR");
     console.log("MY_VAR:", myVar);
-    return this.appService.getHello();
   }
 
   @Get("my-test")
