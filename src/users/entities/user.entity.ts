899db6c0185760ff3a04c3e837b70bcb4a7b13e7
Erick Burgos <eburgosrivas1997@gmail.com>
2026-01-27 00:59:42 -0600

    feat: typeORM entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,23 +1,42 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
 
-@Entity()
+@Entity({
+    name: 'users',
+})
+
 export class UserEntity {
-  @PrimaryGeneratedColumn('uuid')
-  id: number;
+    @PrimaryGeneratedColumn('uuid')
+    id: number;
 
-  @Column()
-  name: string;
+    @Column({
+        type: 'varchar',
+        length: 255,
+    })
+    name: string;
 
-  @Column()
-  email: string;
+    @Column({
+        type: 'varchar',
+        length: 255,
+        unique: true
+    })
+    email: string;
 
-  @Column()
-  password: string;
+    @Column()
+    password: string;
 
-  @Column()
-  createdAt: Date;
+    @CreateDateColumn({
+        type: 'timestamptz',
+        default: () => 'CURRENT_TIMESTAMP',
+        name: 'created_at',
+    })
+    createdAt: Date;
 
-  @Column()
-  updatedAt: Date;
+    @UpdateDateColumn({
+        type: 'timestamptz',
+        default: () => 'CURRENT_TIMESTAMP',
+        onUpdate: 'CURRENT_TIMESTAMP',
+        name: 'updated_at',
+    })
+    updatedAt: Date;
 }
